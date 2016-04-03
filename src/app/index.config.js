40cbad262b8dbdcb619862ad7bbab9bca8c7676a
Mike <mike_zheng0@hotmail.com>
2016-04-03 19:22:24 -0400

    designed the basic layout for the main page

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -3,8 +3,12 @@
 
   angular
     .module('ctrlNewnodb')
-    .config(config);
-
+    .config(config)
+    .config(function($mdIconProvider) {
+      $mdIconProvider
+        .iconSet('social', 'img/icons/sets/social-icons.svg', 24)
+        .defaultIconSet('img/icons/sets/core-icons.svg', 24);
+    });
   /** @ngInject */
   function config($logProvider, toastrConfig) {
     // Enable log
